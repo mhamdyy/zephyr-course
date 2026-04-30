a92dbccf3443c64018fe4cac1534b055c5354840
Mohamed Hamdy <mohamed.hamdy2323@gmail.com>
2026-05-01 01:01:55 +0300

    fix the API calling

--- app/boards/our_board_scratch/board.c
+++ app/boards/our_board_scratch/board.c
@@ -1,8 +1,9 @@
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
+#include <esp_rom_uart.h>
 
 void board_early_init_hook (void)
 {
     /*Some function to run before boot*/
-    printf("Board Initialized");
+    esp_rom_printf("Board Initialized\n");
 }
