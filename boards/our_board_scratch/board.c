1bab7a5821d1bcd9df0be000a1475f52a25e5b0b
Mohamed Hamdy <mohamed.hamdy2323@gmail.com>
2026-05-01 12:24:59 +0300

    Remove the board from scratch

--- boards/our_board_scratch/board.c
+++
@@ -1,9 +0,0 @@
-#include <zephyr/init.h>
-#include <zephyr/kernel.h>
-#include <esp_rom_sys.h>
-
-void board_early_init_hook (void)
-{
-    /*Some function to run before boot*/
-    esp_rom_printf("Board Initialized\n");
-}
