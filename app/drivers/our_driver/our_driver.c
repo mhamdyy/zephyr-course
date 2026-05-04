e8c9e2c96e65f225c12c061205669ee36e789c1d
Mohamed Hamdy <mohamed.hamdy2323@gmail.com>
2026-05-05 02:35:12 +0300

    Adding our Driver as specified in the lecture

---
+++ app/drivers/our_driver/our_driver.c
@@ -0,0 +1,40 @@
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+
+#define DT_DRV_COMPAT our_driver
+
+LOG_MODULE_REGISTER(our_driver, LOG_LEVEL_INF);
+
+static int our_driver_init(const struct device *dev);
+static int our_driver_channel_get(const struct device *dev, enum sensor_channel channel, struct sensor_value* val);
+static int our_driver_sample_fetch(const struct device *dev, enum sensor_channel channel);
+
+static DEVICE_API(sensor, api_our_driver) = {
+    .sample_fetch = our_driver_sample_fetch,
+    .channel_get = our_driver_channel_get,
+};
+
+DEVICE_DT_INST_DEFINE(0, init_our_driver, NULL, NULL, NULL, POST_KERNEL, 80, &api_our_driver);
+
+static const struct our_driver_config cfg_##inst = {
+    .i2c = I2C_DT_SPEC_GET(inst),
+    .calibration = DT_INST_PROP(inst, calibration),
+};
+
+static int our_driver_sample_fetch(const struct device *dev, enum sensor_channel channel)
+{
+    LOG_INF("Hello from our driver sample fetch %d\n", channel);
+    return 0;
+}
+
+static int our_driver_channel_get(const struct device *dev, enum sensor_channel channel, struct sensor_value* val)
+{
+    LOG_INF("Hello from our driver channel get %d\n", channel);
+    return 0;
+}
+
+static int our_driver_init(const struct device *dev)
+{
+    LOG_INF("Hello from our driver init\n");
+    return 0;
+}
