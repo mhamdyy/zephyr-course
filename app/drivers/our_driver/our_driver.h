8bdf3253f44665f4460be48339d16331811d0508
Mohamed Hamdy <mohamed.hamdy2323@gmail.com>
2026-05-05 23:40:15 +0300

    allow the linking between c and cpp files

--- app/drivers/our_driver/our_driver.h
+++ app/drivers/our_driver/our_driver.h
@@ -1,6 +1,14 @@
 #ifndef OUR_DRIVER_H
 #define OUR_DRIVER_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void our_driver_set_param(const struct device *dev, uint32_t param);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* OUR_DRIVER_H */
