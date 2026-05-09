e914201de2612e5efe2f00b018e4105c14831c6b
Mohamed Hamdy <mohamed.hamdy2323@gmail.com>
2026-05-10 01:04:23 +0300

    change the reply of info shell command to be info instead of print

--- app/drivers/our_driver/our_driver_shell.c
+++ app/drivers/our_driver/our_driver_shell.c
@@ -6,10 +6,10 @@ static int shell_our_driver_channel_get(const struct shell* sh, size_t argc, cha
 static int shell_our_driver_info(const struct shell* sh, size_t argc, char** argv);
 
 
-SHELL_STATIC_SUBCMD_SET_CREATE(our_driver_subcmd, 
-    SHELL_CMD_ARG(read,  NULL, "Get channel of the our driver sensor",  shell_our_driver_channel_get,  2, 0),    
-    SHELL_CMD_ARG(fetch, NULL, "Sample fetch of the our driver sensor", shell_our_driver_sample_fetch, 2, 0),    
-    SHELL_CMD_ARG(info,  NULL, "Get info of the our driver sensor",     shell_our_driver_info,         2, 0),    
+SHELL_STATIC_SUBCMD_SET_CREATE(our_driver_subcmd,
+    SHELL_CMD_ARG(read,  NULL, "Get channel of the our driver sensor",  shell_our_driver_channel_get,  2, 0),
+    SHELL_CMD_ARG(fetch, NULL, "Sample fetch of the our driver sensor", shell_our_driver_sample_fetch, 2, 0),
+    SHELL_CMD_ARG(info,  NULL, "Get info of the our driver sensor",     shell_our_driver_info,         2, 0),
     SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(our_driver, &our_driver_subcmd, "Our Driver sensor", NULL);
@@ -18,13 +18,13 @@ static int shell_our_driver_sample_fetch(const struct shell* sh, size_t argc, ch
 {
     int ret = 0;
     const struct device* dev = shell_device_get_binding(argv[1]);
-    
+
     if(NULL == dev)
     {
         shell_error(sh, "Couldn't find device %s", argv[1]);
         return -EFAULT;
     }
-    
+
     ret = sensor_sample_fetch(dev);
 
     return 0;
@@ -35,20 +35,20 @@ static int shell_our_driver_channel_get(const struct shell* sh, size_t argc, cha
     struct sensor_value val;
     int ret = 0;
     const struct device* dev = shell_device_get_binding(argv[1]);
-    
+
     if(NULL == dev)
     {
         shell_error(sh, "Couldn't find device %s", argv[1]);
         return -EFAULT;
     }
-    
+
     ret = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &val);
     if ( 0 != ret)
     {
         shell_error(sh, "Couldn't fetch channel get %d", ret);
         return -EFAULT;
     }
-    
+
     shell_info(sh, "The sensor channel get result is %d", val.val1);
 
     return 0;
@@ -56,8 +56,8 @@ static int shell_our_driver_channel_get(const struct shell* sh, size_t argc, cha
 
 static int shell_our_driver_info(const struct shell* sh, size_t argc, char** argv)
 {
-    shell_print(sh, "The device name is %s", argv[1]);
-    shell_print(sh, "Ready state");
+    shell_info(sh, "The device name is %s", argv[1]);
+    shell_info(sh, "Ready state");
 
     return 0;
 }
